fe24fa12528e798c0b5e21b54eaf6f9bdf5b9c96
Chris Rodriguez <chrizrodz@gmail.com>
2019-05-27 15:27:17 -0500

    New architectures for regime classifier

--- core_logic/RegimeClassifier.py
+++ core_logic/RegimeClassifier.py
@@ -1,19 +1,35 @@
 from models.regime_models.SVMModel import SVMModel
+from models.regime_models.NeuralNetModel_regime import NeuralNetModel_regime
 from helper_scripts.ModelHelper import ModelHelper
+import sklearn.metrics
+import numpy as np
+
+load_model = True	# Load the file from disk
 
 class RegimeClassifier:
 	"""
 	Small adapter class that handles regime prediction
-	For now, we only use SVMs, but this may change in the future.
 	"""
 
-	svm = None
+	neuralnet = None
 
 	def __init__(self):
 		self.MH = ModelHelper.get_instance()
-		self.svm = SVMModel(self.MH.train_features_dat, self.MH.train_regime_dat)
+		self.neuralnet = NeuralNetModel_regime()
+		print("regime classifier")
+		if load_model:
+			print("Loading classifier")
+			self.neuralnet.load_model()
+		else:
+			print("Training classifier")
+			print("Data points: " + str(len(self.MH.train_features_dat)))
+			self.neuralnet.train_model(self.MH.train_features_dat, self.MH.train_regime_dat)
 
+		train_features = np.stack(self.MH.train_features_dat)
+		train_labels = np.stack(self.MH.train_regime_dat)
+		print("Train accuracy: " + str(sklearn.metrics.accuracy_score(train_labels-1,self.neuralnet.classifier_model.predict_classes(train_features))))
+		print()
 
 	def predict(self,features):
-		return self.svm.predict(features)
+		return self.neuralnet.predict(features)
 
